4541dc8ff25dd5d4cb56e3c9731d8c81f8ee6b19
Alexjuh1 <alexjuh1@live.nl>
2016-03-16 10:41:23 +0100

    App
    App toegevoegd

---
+++ App/Assets/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	
+	//WebCamTexture back;
+	public GameObject plane;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		WebCamDevice[] devices = WebCamTexture.devices;
+		//WebCamTexture webcamTexture = new WebCamTexture();
+
+			Debug.Log ("Script has been started");
+			plane = GameObject.FindWithTag ("Player");
+
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+
+
+		}
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
