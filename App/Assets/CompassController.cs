797b9df510ac2432856a2675825661d82138ef4b
Jeroen V <j.vpersonal@hotmail.com>
2016-03-20 17:54:13 +0100

    - Re-upload of App files.

---
+++ App/Assets/CompassController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CompassController : MonoBehaviour 
+{
+	public bool enableTilt = true;
+	Pose3D headview;
+		
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = enableTilt;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		headview = Cardboard.SDK.HeadPose;
+		GetComponent<Text>().text = "Heading: " + headview.Orientation.eulerAngles;
+
+	}
+}
